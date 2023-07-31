101e779e4daea17e5e5088fd90fb76739bfd2f4d
Alex <188hardbass@gmail.com>
2023-07-31 15:14:40 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, inject } from '@angular/core';
+import { LoadingService } from './services/loading.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  public loader$ = inject(LoadingService).loading$;
+}
